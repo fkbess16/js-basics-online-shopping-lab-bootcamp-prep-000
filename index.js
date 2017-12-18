0d9dfa52e22f253ecb7342ac550650a55084b811
Flannery Bessette <fkbess16@gmail.com>
2017-12-18 23:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,9 +47,10 @@ return cart;
 }
 
 function placeOrder(cardNumber) {
+  console.log(cart);
 if (cardNumber === undefined) {console.log ("Sorry, we don't have a credit card on file for you.")} 
 else 
-{console.log (`your total cost is ${total(cart)}, which will be charged to the card ${cardNumber}.`)}
+{console.log (`your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)}
   cart.length = 0;
   return cart;
 }
